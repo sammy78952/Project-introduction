11e0cbf4aa36cccae80a24da7db9968c3f859bce
WilliamKing723 <50292574+WilliamKing723@users.noreply.github.com>
2021-06-15 01:00:30 +0800

    Update bouncingBall.cpp

--- BouncingBall/bouncingBall.cpp
+++ BouncingBall/bouncingBall.cpp
@@ -1,12 +1,19 @@
-﻿#include <stdbool.h>
-#include <studio.h>
-#include <stdlib>
+#include <cstdbool>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
 #include <math.h>
+#include <time.h>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 //視窗属性
 #define WINDOW_WIDTH  640//視窗寬度
 #define WINDOW_HEIGHT  600//視窗長度
+
+//主迴圈延遲
+const int MAIN_LOOP_DELAY = 5;
 
 //位置
 SDL_Rect BlockRect; //方塊
@@ -19,14 +26,33 @@ SDL_Event event;
 
 //指针
 SDL_Window* window = NULL;
-
 SDL_Renderer* renderer = NULL;
+
+//分數
+int score = 0;
+
+//字體
+TTF_Font* font;
+//分數文字
+int scoreFontReslution = 10; //字體解析度
+SDL_Color scoreColor = { 255, 255, 255, 50 }; //字體顏色 {red, green, blue ,alpha}
+SDL_Rect scoreRect = { 540, 500, 40, 80 }; //字體尺寸 {起始x, 起始y, width, height}
+
+//統計文字
+int statisticsFontReslution = 15; //字體解析度
+SDL_Color statisticsTextColor = { 255, 255, 150, 0 }; //字體顏色
+SDL_Rect statisticsTextRect = { 100, 100, 300, 100 }; //字體尺寸
+
+//統計分數
+int statisticsScoreReslution = 10; //字體解析度
+SDL_Color statisticsScoreColor = { 255, 255, 255, 0 }; //字體顏色
+SDL_Rect statisticsScoreRect = { 175, 250, 150, 200 }; //字體尺寸
 
 SDL_Surface* BallSueface = NULL;
 SDL_Surface* BoardSurface = NULL;
 SDL_Surface* Board2Surface = NULL;
-SDL_Surface* Block = NULL;//方塊
-SDL_Surface* breakBlock = NULL;//碎方塊
+SDL_Surface* Block = NULL; //方塊
+SDL_Surface* breakBlock = NULL; //碎方塊
 
 
 SDL_Texture* BlockTT;
@@ -36,61 +62,71 @@ SDL_Texture* BoardTT = NULL;
 SDL_Texture* Board2TT = NULL;
 //結構體
 
-typedef struct SSurface//方塊結構體
+typedef struct SSurface //方塊結構體
 {
-    SDL_Texture* Texture;//文理
-    SDL_Rect TargetRect;//目標rect
-    SDL_Rect SourceRect;//圖片的rect
-    int index;//顯示那張圖片
-    bool state;//是否有效
+    SDL_Texture* Texture; //文理
+    SDL_Rect TargetRect; //目標rect
+    SDL_Rect SourceRect; //圖片的rect
+    int index; //顯示那張圖片
+    bool state; //是否有效
 }SSurface;
-SSurface BlickManager[13][10];//結構體陣列，存放方塊結構體
+SSurface BlickManager[13][10]; //結構體陣列，存放方塊結構體
 
 //參數
-const int BlockW = 64;//方塊的寬度
-const int BlockH = 32;//方塊的高度
+const int BlockW = 64; //方塊的寬度
+const int BlockH = 32; //方塊的高度
+
 //球移動速度
 int BoardSpeed = 3;
-int Board2Speed = 3;//反彈棒移動速度
-int BallSpeed = 2;//球移動速度
+int Board2Speed = 3; //反彈棒移動速度
+int BallSpeed = 2; //球移動速度
 
 //控制反彈棒的移動方向
 bool moveBoardLeft = false;
 bool moveBoardRight = false;
 bool moveBoard2Left = false;
 bool moveBoard2Right = false;
+
 //控制球的移動方向
 bool moveRight = true;
 bool moveDown = true;
-//
-int index;
+
+// 暫停狀態
+bool pause = true;
+
 //子函數
-void init(void);//初始化函數
-int UpdateLogic(void);//更新邏輯
+void init(void); //初始化函數
+int UpdateLogic(void); //更新邏輯
 void randnum();
 void draw(void);
-int check_collision();//檢查球與方塊的碰撞
-bool checkCollision(SDL_Rect A, SDL_Rect B);//球與反彈棒相撞檢測
-bool checkCollision(SDL_Rect A, SDL_Rect C);//球與反彈棒2相撞檢測
-double GetDistance(int x1, int y1, int x2, int y2);//兩點距離
+int check_collision(); //檢查球與方塊的碰撞
+bool checkCollision(SDL_Rect A, SDL_Rect B); //球與反彈棒相撞檢測
+bool checkCollision(SDL_Rect A, SDL_Rect C); //球與反彈棒2相撞檢測
+double GetDistance(int x1, int y1, int x2, int y2); //兩點距離
 
 
 
 int main(int argc, char* argv[])
 {
     init();//初始化
-    bool quit = false;
-    while (quit == false)
+    srand(time(NULL));
+    randnum();
+    while (window)
     {
         while (SDL_PollEvent(&event))
         {
 
             if (event.type == SDL_QUIT)//退出事件
             {
-                quit = true;
+                SDL_DestroyWindow(window);
+                window = NULL;
             }
             else if (event.type == SDL_KEYDOWN)//鍵盤事件
             {
+                if (event.key.keysym.sym == SDLK_SPACE || event.key.keysym.sym == SDLK_RETURN) //暫停
+                {
+                    pause = !pause;
+                }
                 if (event.key.keysym.sym == SDLK_LEFT)//左移動
                 {
                     moveBoardLeft = true;
@@ -145,33 +181,67 @@ int main(int argc, char* argv[])
                     Board2Speed = 3;
                 }
             }
-
-            randnum();
         }
-
-
-        if (UpdateLogic() == 1) {
-            UpdateLogic();//更新邏輯
-
+        if (!pause) {
+            if (!UpdateLogic())
+            {
+                cout << "Game over" << std::endl;
+                cout << "Your score is: " << score/2 << std::endl;
+                SDL_DestroyWindow(window);
+                window = NULL;
+            }
         }
-        else {            
-            quit = true;
-        }
-
-        SDL_RenderClear(renderer);
         draw();
-        SDL_RenderPresent(renderer);//刷新
-        SDL_Delay(10);//釋放CPU
+         SDL_Delay(MAIN_LOOP_DELAY);//加入一點延遲，防止CPU耗盡
     }
+    // 統計視窗 //HERE
+    window = SDL_CreateWindow("Bouncing Ball => Your score!", 100, 100, 500, 500, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    while (window) {
+        while (SDL_PollEvent(&event))
+        {
+
+            if (event.type == SDL_QUIT)//退出事件
+            {
+                SDL_DestroyWindow(window);
+                window = NULL;
+            }
+            else if (event.type == SDL_KEYDOWN)//鍵盤事件
+            {
+                if (event.key.keysym.sym == SDLK_SPACE || event.key.keysym.sym == SDLK_RETURN) //暫停
+                {
+                    SDL_DestroyWindow(window);
+                    window = NULL;
+                }
+            }
+        }
+
+        // 繪製統計文字
+        SDL_Surface* statisticsTextSurface = TTF_RenderText_Solid(font, "Your score", statisticsTextColor);
+        SDL_Texture* statisticsTextTexture = SDL_CreateTextureFromSurface(renderer, statisticsTextSurface);
+        SDL_RenderCopy(renderer, statisticsTextTexture, NULL, &statisticsTextRect);
+        SDL_FreeSurface(statisticsTextSurface);
+        SDL_DestroyTexture(statisticsTextTexture);
+
+        // 繪製統計分數
+        SDL_Surface* statisticScoreSurface = TTF_RenderText_Solid(font, std::to_string(score / 2).c_str(), statisticsScoreColor);
+        SDL_Texture* statisticScoreTexture = SDL_CreateTextureFromSurface(renderer, statisticScoreSurface);
+        SDL_RenderCopy(renderer, statisticScoreTexture, NULL, &statisticsScoreRect);
+        SDL_FreeSurface(statisticScoreSurface);
+        SDL_DestroyTexture(statisticScoreTexture);
+
+        SDL_RenderPresent(renderer);//刷新
+        SDL_Delay(MAIN_LOOP_DELAY);//加入一點延遲，防止CPU耗盡
+    }
+
     SDL_FreeSurface(Block);
     SDL_FreeSurface(BallSueface);
-    SDL_DestroyWindow(window);
-    printf("Game over");
     SDL_Quit();
 
 
     return 0;
 }
+
 
 bool checkCollision(SDL_Rect A, SDL_Rect B)
 {
@@ -249,7 +319,7 @@ bool checkCollision2(SDL_Rect A, SDL_Rect C)
     return true;
 }
 
-int UpdateLogic(void)
+int UpdateLogic()
 {
     //更新圖片，如果index=2,代表碰撞，更換成碎方塊
     int x, y;
@@ -379,25 +449,30 @@ int UpdateLogic(void)
 }
 
 //隨機產生方塊
-void randnum() {
+void randnum(void) {
     int x, y;
-    int max = 9, min = 0;
+    x = (rand() % 8) + 5;
+    y = (rand() % 10);
+    printf("x=%d y=%d ", x, y);
 
-    x = rand() % (max - min + 1) + min;
-    y = rand() % (max - min + 1) + min;
+    BlickManager[x][y].Texture = BlockTT;
 
+    BlickManager[x][y].SourceRect = BlockRect;
+
+    BlickManager[x][y].TargetRect.x = BlockW * y;
+    BlickManager[x][y].TargetRect.y = BlockH * x;
+    BlickManager[x][y].TargetRect.w = BlockRect.w;
+    BlickManager[x][y].TargetRect.h = BlockRect.h;
+    BlickManager[x][y].state = true;
     BlickManager[x][y].index = 1;
-
-
-
 }
-
 void draw(void)
 {
+    SDL_RenderClear(renderer);
     int i, j;
     for (i = 5; i < 13; i++)
     {
-        for (j = 0; j < 10; j++)
+        for (j = 0; j < 10; j++) 
         {
             if (BlickManager[i][j].state == true)
             {
@@ -409,15 +484,25 @@ void draw(void)
     SDL_RenderCopy(renderer, BallTT, NULL, &BallRect);
     SDL_RenderCopy(renderer, BoardTT, NULL, &BoardRect);
     SDL_RenderCopy(renderer, Board2TT, NULL, &Board2Rect);
+
+    SDL_Surface* scoreSurface = TTF_RenderText_Solid(font, std::to_string(score/2).c_str(), scoreColor);
+    SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(renderer, scoreSurface);
+    SDL_RenderCopy(renderer, scoreTexture, NULL, &scoreRect);
+
+    SDL_FreeSurface(scoreSurface);
+    SDL_DestroyTexture(scoreTexture);
+
+    SDL_RenderPresent(renderer);//刷新
 }
 
-void init(void)
+void init()
 {
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+    
     window = SDL_CreateWindow("Bouncing Ball", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_RenderClear(renderer);
 
     //載入圖片
     BallSueface = IMG_Load("C:/Project_image/ballgrey.bmp");//球
@@ -439,6 +524,13 @@ void init(void)
     BlockTT = SDL_CreateTextureFromSurface(renderer, Block);
 
     breakBlockTT = SDL_CreateTextureFromSurface(renderer, breakBlock);
+    
+    //載入文字
+    font = TTF_OpenFont("C:/Project/ttf/sans.ttf", scoreFontReslution);
+    if (!font) {
+        std::cout << "Font is not loaded, error shows below!" << std::endl;
+        std::cout << TTF_GetError() << std::endl;
+    }
 
     //初始位置
     BallRect.x = WINDOW_WIDTH / 2 - (BallSueface->w / 2);
@@ -476,7 +568,7 @@ void init(void)
             BlickManager[i][j].TargetRect.w = BlockRect.w;
             BlickManager[i][j].TargetRect.h = BlockRect.h;
 
-            BlickManager[i][j].state = true;
+            BlickManager[i][j].state = false;
             BlickManager[i][j].index = 2;
         }
     }
