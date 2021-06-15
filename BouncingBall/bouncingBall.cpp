5d2dc711b1c1d588a6799ddadcb03aeff036b68a
babubibo <55521961+babubibo@users.noreply.github.com>
2021-06-15 17:04:12 +0800

    Stable version

--- BouncingBall/bouncingBall.cpp
+++ BouncingBall/bouncingBall.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <cstdlib>
 #include <cmath>
-#include <math.h>
 #include <time.h>
 #include <SDL.h>
 #include <SDL_image.h>
@@ -11,6 +10,7 @@
 //視窗属性
 #define WINDOW_WIDTH  640//視窗寬度
 #define WINDOW_HEIGHT  600//視窗長度
+using namespace std;
 
 //主迴圈延遲
 const int MAIN_LOOP_DELAY = 5;
@@ -19,7 +19,7 @@ const int MAIN_LOOP_DELAY = 5;
 SDL_Rect BlockRect; //方塊
 SDL_Rect BallRect; //球
 SDL_Rect BoardRect; //反彈棒
-SDL_Rect Board2Rect; //反彈棒2
+SDL_Rect Board2Rect;//反彈棒2
 
 //事件
 SDL_Event event;
@@ -33,26 +33,27 @@ int score = 0;
 
 //字體
 TTF_Font* font;
+
 //分數文字
-int scoreFontReslution = 10; //字體解析度
-SDL_Color scoreColor = { 255, 255, 255, 50 }; //字體顏色 {red, green, blue ,alpha}
-SDL_Rect scoreRect = { 540, 500, 40, 80 }; //字體尺寸 {起始x, 起始y, width, height}
+int scoreFontReslution = 50; //字體解析度
+SDL_Color scoreColor = { 255, 255, 255, 0 }; //字體顏色 {red, green, blue ,alpha}
+SDL_Rect scoreRect = { 540, 500, 60, 80 }; //字體尺寸 {起始x, 起始y, width, height}
 
 //統計文字
-int statisticsFontReslution = 15; //字體解析度
+int statisticsFontReslution = 50; //字體解析度
 SDL_Color statisticsTextColor = { 255, 255, 150, 0 }; //字體顏色
 SDL_Rect statisticsTextRect = { 100, 100, 300, 100 }; //字體尺寸
 
 //統計分數
-int statisticsScoreReslution = 10; //字體解析度
+int statisticsScoreReslution = 50; //字體解析度
 SDL_Color statisticsScoreColor = { 255, 255, 255, 0 }; //字體顏色
 SDL_Rect statisticsScoreRect = { 175, 250, 150, 200 }; //字體尺寸
 
 SDL_Surface* BallSueface = NULL;
 SDL_Surface* BoardSurface = NULL;
 SDL_Surface* Board2Surface = NULL;
-SDL_Surface* Block = NULL; //方塊
-SDL_Surface* breakBlock = NULL; //碎方塊
+SDL_Surface* Block = NULL;//方塊
+SDL_Surface* breakBlock = NULL;//碎方塊
 
 
 SDL_Texture* BlockTT;
@@ -62,24 +63,24 @@ SDL_Texture* BoardTT = NULL;
 SDL_Texture* Board2TT = NULL;
 //結構體
 
-typedef struct SSurface //方塊結構體
+typedef struct SSurface//方塊結構體
 {
-    SDL_Texture* Texture; //文理
-    SDL_Rect TargetRect; //目標rect
-    SDL_Rect SourceRect; //圖片的rect
-    int index; //顯示那張圖片
-    bool state; //是否有效
+    SDL_Texture* Texture;//文理
+    SDL_Rect TargetRect;//目標rect
+    SDL_Rect SourceRect;//圖片的rect
+    int index;//顯示那張圖片
+    bool state;//是否有效
 }SSurface;
-SSurface BlickManager[13][10]; //結構體陣列，存放方塊結構體
+SSurface BlickManager[13][10];//結構體陣列，存放方塊結構體
 
 //參數
-const int BlockW = 64; //方塊的寬度
-const int BlockH = 32; //方塊的高度
+const int BlockW = 64;//方塊的寬度
+const int BlockH = 32;//方塊的高度
 
 //球移動速度
 int BoardSpeed = 3;
-int Board2Speed = 3; //反彈棒移動速度
-int BallSpeed = 2; //球移動速度
+int Board2Speed = 3;//反彈棒移動速度
+int BallSpeed = 2;//球移動速度
 
 //控制反彈棒的移動方向
 bool moveBoardLeft = false;
@@ -95,22 +96,27 @@ bool moveDown = true;
 bool pause = true;
 
 //子函數
-void init(void); //初始化函數
-int UpdateLogic(void); //更新邏輯
-void randnum();
-void draw(void);
-int check_collision(); //檢查球與方塊的碰撞
-bool checkCollision(SDL_Rect A, SDL_Rect B); //球與反彈棒相撞檢測
-bool checkCollision(SDL_Rect A, SDL_Rect C); //球與反彈棒2相撞檢測
-double GetDistance(int x1, int y1, int x2, int y2); //兩點距離
-
+void init();//初始化函數
+int UpdateLogic();//更新邏輯
+void randnum();//隨機產生方塊
+void draw();
+int check_collision();//檢查球與方塊的碰撞
+bool checkCollision(SDL_Rect A, SDL_Rect B);//球與反彈棒相撞檢測
+bool checkCollision(SDL_Rect A, SDL_Rect C);//球與反彈棒相撞檢測2
+double GetDistance(int x1, int y1, int x2, int y2);//兩點距離
+bool scorefix = false;//碰撞分數修正
+int scorefixpt = 0;//重複記分修正
 
 
 int main(int argc, char* argv[])
 {
+
     init();//初始化
     srand(time(NULL));
     randnum();
+    randnum();
+    int scorefixpt = 0;
+
     while (window)
     {
         while (SDL_PollEvent(&event))
@@ -125,6 +131,7 @@ int main(int argc, char* argv[])
             {
                 if (event.key.keysym.sym == SDLK_SPACE || event.key.keysym.sym == SDLK_RETURN) //暫停
                 {
+
                     pause = !pause;
                 }
                 if (event.key.keysym.sym == SDLK_LEFT)//左移動
@@ -145,11 +152,11 @@ int main(int argc, char* argv[])
                     moveBoard2Right = true;
                 }
 
-                if (event.key.keysym.sym == SDLK_UP)//加速
+                if (event.key.keysym.sym == SDLK_UP)//反彈棒加速
                 {
                     BoardSpeed = 5;
                 }
-                if (event.key.keysym.sym == SDLK_w)//加速
+                if (event.key.keysym.sym == SDLK_w)//反彈棒2加速
                 {
                     Board2Speed = 5;
                 }
@@ -181,19 +188,24 @@ int main(int argc, char* argv[])
                     Board2Speed = 3;
                 }
             }
+
+
         }
+
         if (!pause) {
             if (!UpdateLogic())
             {
                 cout << "Game over" << std::endl;
-                cout << "Your score is: " << score/2 << std::endl;
+                cout << "Your score is: " << score << std::endl;
                 SDL_DestroyWindow(window);
                 window = NULL;
             }
         }
+
         draw();
-         SDL_Delay(MAIN_LOOP_DELAY);//加入一點延遲，防止CPU耗盡
+        SDL_Delay(MAIN_LOOP_DELAY);//加入一點延遲，防止CPU耗盡
     }
+
     // 統計視窗 //HERE
     window = SDL_CreateWindow("Bouncing Ball => Your score!", 100, 100, 500, 500, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -224,7 +236,7 @@ int main(int argc, char* argv[])
         SDL_DestroyTexture(statisticsTextTexture);
 
         // 繪製統計分數
-        SDL_Surface* statisticScoreSurface = TTF_RenderText_Solid(font, std::to_string(score / 2).c_str(), statisticsScoreColor);
+        SDL_Surface* statisticScoreSurface = TTF_RenderText_Solid(font, std::to_string(score).c_str(), statisticsScoreColor);
         SDL_Texture* statisticScoreTexture = SDL_CreateTextureFromSurface(renderer, statisticScoreSurface);
         SDL_RenderCopy(renderer, statisticScoreTexture, NULL, &statisticsScoreRect);
         SDL_FreeSurface(statisticScoreSurface);
@@ -241,7 +253,6 @@ int main(int argc, char* argv[])
 
     return 0;
 }
-
 
 bool checkCollision(SDL_Rect A, SDL_Rect B)
 {
@@ -278,6 +289,7 @@ bool checkCollision(SDL_Rect A, SDL_Rect B)
     {
         return false;
     }
+    
     return true;
 }
 
@@ -316,12 +328,13 @@ bool checkCollision2(SDL_Rect A, SDL_Rect C)
     {
         return false;
     }
+    
     return true;
 }
 
 int UpdateLogic()
 {
-    //更新圖片，如果index=2,代表碰撞，更換成碎方塊
+    //更新圖片，如果index=2,代表碰撞，更換碎方塊
     int x, y;
     for (x = 5; x < 13; x++)
     {
@@ -369,46 +382,71 @@ int UpdateLogic()
     if (true == moveRight)
     {
         BallRect.x = BallRect.x + BallSpeed;
-        if ((BallRect.x + BallRect.w >= WINDOW_WIDTH) || (1 == check_collision()))
+        if (BallRect.x + BallRect.w >= WINDOW_WIDTH)
         {
             moveRight = false;
             //printf("trun left\n");
+            scorefixpt = 0;
+            scorefix = false;
+        }
+        else if (1 == check_collision())
+        {
+            moveRight = false;
         }
     }
     else
     {
         BallRect.x = BallRect.x - BallSpeed;
-        if ((BallRect.x <= 0) || (2 == check_collision()))
+        if (BallRect.x <= 0)
         {
             moveRight = true;
             //printf("trun right\n");
+            scorefixpt = 0;
+            scorefix = false;
+        }
+        else if (2 == check_collision())
+        {
+            moveRight = true;
         }
     }
 
     if (true == moveDown)
     {
         BallRect.y = BallRect.y + BallSpeed;
-        if (BallRect.y + BallRect.h >= WINDOW_HEIGHT) {
+        if (BallRect.y + BallRect.h >= WINDOW_HEIGHT)
+        {
             SDL_Quit();
             return 0;
         }
-        if (checkCollision(BallRect, BoardRect) || (3 == check_collision()))
+        if (checkCollision(BallRect, BoardRect))
         {
             moveDown = false;
             //printf("trun up\n");
+            scorefix = false;
+            scorefixpt = 0;
+        }
+        else if (3 == check_collision())
+        {
+            moveDown = false;
         }
     }
     else
     {
         BallRect.y = BallRect.y - BallSpeed;
-        if (BallRect.y <= 0) {                      
+        if (BallRect.y <= 0) {
             SDL_Quit();
             return 0;
         }
-        if (checkCollision(BallRect, Board2Rect) || 4 == check_collision())
+        if (checkCollision(BallRect, Board2Rect))
         {
             moveDown = true;
             //printf("trun down\n");
+            scorefix = false;
+            scorefixpt = 0;
+        }
+        else if (4 == check_collision())
+        {
+            moveDown = true;
         }
     }
 
@@ -419,7 +457,6 @@ int UpdateLogic()
         {
             BoardRect.x = BoardRect.x - BoardSpeed;
         }
-
     }
     if (moveBoardRight == true)//反彈棒右移
     {
@@ -427,7 +464,6 @@ int UpdateLogic()
         {
             BoardRect.x = BoardRect.x + BoardSpeed;
         }
-
     }
 
     if (moveBoard2Left == true)//反彈棒左移
@@ -436,7 +472,6 @@ int UpdateLogic()
         {
             Board2Rect.x = Board2Rect.x - Board2Speed;
         }
-
     }
     if (moveBoard2Right == true)//反彈棒右移
     {
@@ -451,28 +486,57 @@ int UpdateLogic()
 //隨機產生方塊
 void randnum(void) {
     int x, y;
-    x = (rand() % 8) + 5;
-    y = (rand() % 10);
-    printf("x=%d y=%d ", x, y);
+    
+    x = (rand() % 8) + 5;//x[5,13]
+    y = (rand() % 10);//y[0,10]
+       
+    //printf("x=%d y=%d \n", x, y);
+    if (BlockRect.x == WINDOW_WIDTH / 2 - (BallSueface->w / 2) && BlockRect.y == WINDOW_HEIGHT / 10 * 9 - BallSueface->h)
+    {     
+        BlickManager[x][y].state = false;
+        BlickManager[x][y].index = 2;
+        //printf("ERROR");
+    }
+    else
+    {     
+        BlickManager[x][y].Texture = BlockTT;
 
-    BlickManager[x][y].Texture = BlockTT;
+        BlickManager[x][y].SourceRect = BlockRect;
 
-    BlickManager[x][y].SourceRect = BlockRect;
+        BlickManager[x][y].TargetRect.x = BlockW * y;
+        BlickManager[x][y].TargetRect.y = BlockH * x;
+        BlickManager[x][y].TargetRect.w = BlockRect.w;
+        BlickManager[x][y].TargetRect.h = BlockRect.h;
+        BlickManager[x][y].state = true;
+        BlickManager[x][y].index = 1;      
+    }
+    if (y % 2 == 1)
+    {
+        x = (rand() % 8) + 5;
+        y = (rand() % 10);
 
-    BlickManager[x][y].TargetRect.x = BlockW * y;
-    BlickManager[x][y].TargetRect.y = BlockH * x;
-    BlickManager[x][y].TargetRect.w = BlockRect.w;
-    BlickManager[x][y].TargetRect.h = BlockRect.h;
-    BlickManager[x][y].state = true;
-    BlickManager[x][y].index = 1;
+        BlickManager[x][y].Texture = BlockTT;
+
+        BlickManager[x][y].SourceRect = BlockRect;
+
+        BlickManager[x][y].TargetRect.x = BlockW * y;
+        BlickManager[x][y].TargetRect.y = BlockH * x;
+        BlickManager[x][y].TargetRect.w = BlockRect.w;
+        BlickManager[x][y].TargetRect.h = BlockRect.h;
+        BlickManager[x][y].state = true;
+        BlickManager[x][y].index = 1;
+        //printf("random x=%d y=%d \n", x, y);
+        //printf("random!!!");
+    }
 }
+
 void draw(void)
 {
     SDL_RenderClear(renderer);
     int i, j;
     for (i = 5; i < 13; i++)
     {
-        for (j = 0; j < 10; j++) 
+        for (j = 0; j < 10; j++)
         {
             if (BlickManager[i][j].state == true)
             {
@@ -485,7 +549,7 @@ void draw(void)
     SDL_RenderCopy(renderer, BoardTT, NULL, &BoardRect);
     SDL_RenderCopy(renderer, Board2TT, NULL, &Board2Rect);
 
-    SDL_Surface* scoreSurface = TTF_RenderText_Solid(font, std::to_string(score/2).c_str(), scoreColor);
+    SDL_Surface* scoreSurface = TTF_RenderText_Solid(font, std::to_string(score).c_str(), scoreColor);
     SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(renderer, scoreSurface);
     SDL_RenderCopy(renderer, scoreTexture, NULL, &scoreRect);
 
@@ -499,10 +563,11 @@ void init()
 {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
-    
+
     window = SDL_CreateWindow("Bouncing Ball", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    //SDL_RenderClear(renderer);
 
     //載入圖片
     BallSueface = IMG_Load("images/ballgrey.bmp");//球
@@ -524,13 +589,9 @@ void init()
     BlockTT = SDL_CreateTextureFromSurface(renderer, Block);
 
     breakBlockTT = SDL_CreateTextureFromSurface(renderer, breakBlock);
-    
+
     //載入文字
-    font = TTF_OpenFont("C:/Project/ttf/sans.ttf", scoreFontReslution);
-    if (!font) {
-        std::cout << "Font is not loaded, error shows below!" << std::endl;
-        std::cout << TTF_GetError() << std::endl;
-    }
+    font = TTF_OpenFont("ttf/OpenSans-Bold.ttf", scoreFontReslution);//字體
 
     //初始位置
     BallRect.x = WINDOW_WIDTH / 2 - (BallSueface->w / 2);
@@ -597,7 +658,6 @@ int check_collision()
         {
             if (BlickManager[i][j].state == true)//如果state為真，判斷是否碰撞
             {
-
                 //尋找最近的X位置
                 if (cx < BlickManager[i][j].TargetRect.x)
                 {
@@ -629,36 +689,70 @@ int check_collision()
                 if (GetDistance(cx, cy, x, y) <= cr)//當兩點距離小於半徑，發生碰撞
                 {
                     //BlickManager[i][j].state = false;
-                    BlickManager[i][j].index = 2;//把方塊設置成碎方塊
-                    ++score; //加分!
+                    BlickManager[i][j].index = 2;//把方塊設置成碎方塊    
+
                     if (cx < x && y >= BlickManager[i][j].TargetRect.y && y <= BlickManager[i][j].TargetRect.y + BlockH)//碰到左邊
                     {
-                        //printf("touch left\n");
+                        //printf("left test");
+                        if ((scorefixpt != 1) || (scorefix == false))//避免重複碰撞
+                        {
+                            randnum();
+                            scorefix = true;
+                            scorefixpt = 1;
+                            ++score;//加分
+                            //printf("touch left\n");
+                        }
                         return 1;
                     }
                     else if (cx > x && y >= BlickManager[i][j].TargetRect.y && y <= BlickManager[i][j].TargetRect.y + BlockH)//碰到右邊
                     {
-                        //printf("touch right\n");
+
+                        //printf("right test");
+                        if (scorefixpt != 2 || (scorefix == false))//避免重複碰撞
+                        {
+                            randnum();
+                            scorefix = true;
+                            scorefixpt = 2;
+                            ++score;//加分
+                            //printf("touch right\n");
+                        }
                         return 2;
                     }
                     else if (cy < y && x >= BlickManager[i][j].TargetRect.x && x <= BlickManager[i][j].TargetRect.x + BlockW)//碰到上邊
                     {
-                        //printf("touch top\n");
+                        //printf("top test");
+                        if (scorefixpt != 3)//避免重複碰撞
+                        {
+                            randnum();
+                            scorefixpt = 3;
+
+                            ++score;//加分
+                            //printf("touch top\n");
+
+                        }
                         return 3;
                     }
                     else if (cy > y && x >= BlickManager[i][j].TargetRect.x && x <= BlickManager[i][j].TargetRect.x + BlockW)//碰到下邊
                     {
-                        //printf("touch button\n");
+
+                        //printf("bottom test");
+                        if (scorefixpt != 4 )//避免重複碰撞
+                        {
+                            randnum();
+                            scorefixpt = 4;
+                            ++score;//加分
+                            //printf("touch button\n");
+                        }
                         return 4;
                     }
                     else
                     {
-                        //printf("x=%d y=%d\n",x,y);
+                        //printf("x = %d, y = %d\n",x,y);
                     }
                 }
             }
         }
     }
-
     return 0;
 }
+
